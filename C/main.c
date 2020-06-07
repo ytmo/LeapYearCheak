4a41c485d80e3c404aba832845c0fc3b4d922d93
TomsProject <44722363+TomsProject@users.noreply.github.com>
2020-06-07 11:14:41 +0800

    Create main.c

---
+++ C/main.c
@@ -0,0 +1,29 @@
+//
+//  main.c
+//  Leap Year Cheak
+//  C Version
+//
+//  Created by Toms Project on 2020/6/7.
+//  Copyright © 2020 Toms Project. All rights reserved.
+//
+
+#include <stdio.h>
+//#include <stdlib.h>                                 //for using SYSTEM
+
+int main()
+{
+    int year;
+    printf("Please input a year, and I'll check is that a leap year!!!\n");
+    scanf("%d",&year);                             //input the year we want to check
+    if(year%4==0 && year%100!=0 || year%400==0)    //we learned from math that if a year remainder 4 equals 0 and that year remainder 100 don't equals 0 or the year remainder 400 equals 0, than the year must be a leap year
+    {
+        printf("%d is a leap year!\n",year);
+    }
+    else
+    {
+        printf("%d is not a leap year!\n",year);
+    }
+    //system("pause");                              //to see result clearly
+    return 0;
+}
+
