6b621bb5eba473ceff8280dc25c36da450be2a9e
TomsProject <44722363+TomsProject@users.noreply.github.com>
2020-06-10 17:21:22 +0800

    Beautfully files

--- C/main.c
+++ C/main.c
@@ -14,18 +14,18 @@ int main()
 {
     int year;
     printf("Please input a year, and I'll check is that a leap year!!!\n");
-    scanf("%d",&year);                             //input the year we want to check
+    scanf("%d", &year);                             //input the year we want to check
     if(year % 4 == 0 && year % 100 != 0)    //we learned from math that if a year remainder 4 equals 0 and that year remainder 100 don't equals 0 or the year remainder 400 equals 0, than the year must be a leap year
     {
-        printf("%d is a leap year!\n",year);
+        printf("%d is a leap year!\n", year);
     }
     else if (year % 400 == 0)
     {
-        printf("%d is a leap year!\n",year);
+        printf("%d is a leap year!\n", year);
     }
     else
     {
-        printf("%d is not a leap year!\n",year);
+        printf("%d is not a leap year!\n", year);
     }
     //system("pause");                              //to see result clearly
     return 0;
