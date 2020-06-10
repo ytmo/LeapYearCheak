c13149eb23ade4efc70a329820f14562b484519d
TomsProject <44722363+TomsProject@users.noreply.github.com>
2020-06-10 16:48:03 +0800

    [Err Fix] Warning: Format string is not a string literal (potentially insecure)

--- Cpp/main.cpp
+++ Cpp/main.cpp
@@ -19,7 +19,7 @@ int main(int argc, const char * argv[])
 {
     char buf[500];
     string chose;
-    sprintf(buf, argv[1]);
+    sprintf(buf, "%s", argv[1]);
     chose = buf;
     if (chose == "-h" || chose == "-help" || chose == "--h" || chose == "--help")
     {
@@ -39,7 +39,7 @@ int main(int argc, const char * argv[])
     }
     else if (chose == "-s" || chose == "-suan" || chose == "--s" || chose == "--suan")
     {
-        sprintf(buf, argv[2]);
+        sprintf(buf, "%s", argv[2]);
         auto year = atoll(buf);//input the year we want to check
         if(year % 4 == 0 && year % 100 != 0)    //we learned from math that if a year remainder 4 equals 0 and that year remainder 100 don't equals 0 or the year remainder 400 equals 0, than the year must be a leap year
         {
