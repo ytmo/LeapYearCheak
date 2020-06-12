7a568772da447b7c41181d2316e49d18439f0bb4
TomsProject <44722363+TomsProject@users.noreply.github.com>
2020-06-12 21:45:46 +0800

    Create main.swift

---
+++ Swift/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  swiftleapyear
+//
+//  Created by Toms Project on 2020/6/12.
+//  Copyright © 2020 Toms Project. All rights reserved.
+//
+
+import Foundation     //for print and input
+
+print("Please input an number, and I'll check it!")
+//NO INPUT WAS FOUND
+
+
