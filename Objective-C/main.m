ccc702b34a09d2f76198eeb48ba45cba034f1be9
TomsProject <44722363+TomsProject@users.noreply.github.com>
2020-06-13 14:55:16 +0800

    Update main.m

--- Objective-C/main.m
+++ Objective-C/main.m
@@ -1,7 +1,16 @@
+//
+//  main.m
+//  LeapYearCheak
+//
+//  Created by Toms Project on 2020/6/13.
+//  Copyright © 2020 Toms Project. All rights reserved.
+//
+
 #import <Foundation/Foundation.h>
-int main(void)
-{
-    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-    NSLog(@"Hello, world!");
-    [pool drain];
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"Hello, World!");
+    }
+    return 0;
 }
