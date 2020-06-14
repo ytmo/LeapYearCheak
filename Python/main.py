2efadcf54df7fde87025f5b30b7815440f6adc27
TomsProject <44722363+TomsProject@users.noreply.github.com>
2020-06-14 22:37:57 +0800

    [Still Error]

--- Python/main.py
+++ Python/main.py
@@ -1,7 +1,8 @@
 year = input("Please input a year, and I'll check is that a leap year!!!")
-if year % 4 == 0 && year % 100 != 0:    #we learned from math that if a year remainder 4 equals 0 and that year remainder 100 don't equals 0 or the year remainder 400 equals 0, than the year must be a leap year
-	print(year + " is a leap year!");
-else if year % 400 == 0:
+if year % 4 == 0:    #we learned from math that if a year remainder 4 equals 0 and that year remainder 100 don't equals 0 or the year remainder 400 equals 0, than the year must be a leap year
+	if year % 100 != 0:
+    	print(year + " is a leap year!");
+elif year % 400 == 0:
     print(year + " is a leap year!");
 else:
     print(year + " is not a leap year!");
