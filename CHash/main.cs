056f390f0bc1c546428c4dc8bfd69d2d840f5033
TomsProject <44722363+TomsProject@users.noreply.github.com>
2020-06-14 22:06:24 +0800

    [Success]

--- CHash/main.cs
+++ CHash/main.cs
@@ -1,8 +1,21 @@
 using System;
 
-public class HelloWorld{
+public class LeapYearCheak{
     private static void Main(){
-        Console.WriteLine("Hello From C# Mono!!!");
+        Console.WriteLine("Please input a year, and I'll check is that a leap year!!!");
+        int year = int.Parse(Console.ReadLine());
+        if(year % 4 == 0 && year % 100 != 0)    //we learned from math that if a year remainder 4 equals 0 and that year remainder 100 don't equals 0 or the year remainder 400 equals 0, than the year must be a leap year
+    	{
+        	Console.WriteLine(year + " is a leap year!");
+    	}
+    	else if (year % 400 == 0)
+    	{
+    	    Console.WriteLine(year + " is a leap year!");
+    	}
+    	else
+    	{
+    	    Console.WriteLine(year + " is not a leap year!");
+    	}
     }
 }
     
