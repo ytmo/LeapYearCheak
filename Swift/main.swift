bfbd553be335fbdc60cac227e950d7d8f9e640a2
TomsProject <44722363+TomsProject@users.noreply.github.com>
2020-06-14 22:31:48 +0800

    Update main.swift

--- Swift/main.swift
+++ Swift/main.swift
@@ -1,14 +1,27 @@
 //
 //  main.swift
-//  swiftleapyear
+//  LeapYearCheak
 //
-//  Created by Toms Project on 2020/6/12.
+//  Created by Toms Project on 2020/6/14.
 //  Copyright © 2020 Toms Project. All rights reserved.
 //
 
 import Foundation     //for print and input
 
-print("Please input an number, and I'll check it!")
-//NO INPUT WAS FOUND
+print("Please input a year, and I'll check is that a leap year!!!")
+let year = Int(readLine()!)
+if(year! % 4 == 0 && year! % 100 != 0)    //we learned from math that if a year remainder 4 equals 0 and that year remainder 100 don't equals 0 or the year remainder 400 equals 0, than the year must be a leap year
+{
+	print(String(year!) + " is a leap year!")
+}
+else if (year! % 400 == 0)
+{
+    print(String(year!) + " is a leap year!")
+}
+else
+{
+    print(String(year!) + " is not a leap year!")
+}
+    
 
 
