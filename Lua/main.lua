eb01dde8e513c5e7694a3245ac6c7f19e3b1af25
TomsProject <44722363+TomsProject@users.noreply.github.com>
2020-06-15 20:09:04 +0800

    [Success but wrong \n]

---
+++ Lua/main.lua
@@ -0,0 +1,19 @@
+print("Please input a year, and I'll check is that a leap year!!!")
+year = io.read('*n')
+--we learned from math that if a year remainder 4 equals 0 and that year remainder 100 don't equals 0 or the year remainder 400 equals 0, than the year must be a leap year
+if (year % 4 == 0)
+then
+	if (year % 100 ~= 0)
+    then
+    	--print(tostring(year) + " is a leap year!")
+        print(year)
+        print(" is a leap year!")
+	end
+elseif (year % 400 == 0)
+then
+    print(year)
+    print(" is a leap year!")
+else
+    print(year)
+    print(" is not a leap year!")
+end
