6e5d36b9290de30fa0b3a77bdcbbd59ed2aa4a44
TomsProject <44722363+TomsProject@users.noreply.github.com>
2020-06-25 21:07:12 +0800

    [Add lost \n but still wrong \n]

--- Perl/main.pl
+++ Perl/main.pl
@@ -4,11 +4,11 @@ $year = <STDIN>;
 if( $year % 4 == 0 ) {
 	if( $year % 100 != 0 )
     {
-    	print("$year is a leap year!");
+    	print("$year is a leap year!\n");
     }
 } elsif ($year % 400 == 0) {
-    print("$year is a leap year!");
+    print("$year is a leap year!\n");
 } else {
-    print("$year is not a leap year!");
+    print("$year is not a leap year!\n");
 }
 
