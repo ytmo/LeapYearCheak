b9a7472c0f95fabbb600ecea22d9a595e69c42c4
TomsProject <44722363+TomsProject@users.noreply.github.com>
2020-06-25 21:01:09 +0800

    [Fix wrong outputs that caused by typo]
    [Fix wrong outputs that caused by typo but still wrong \n]

--- Perl/main.pl
+++ Perl/main.pl
@@ -1,10 +1,14 @@
 print "Please input a year, and I'll check is that a leap year!!! \n";
 $year = <STDIN>;
 #we learned from math that if a year remainder 4 equals 0 and that year remainder 100 don't equals 0 or the year remainder 400 equals 0, than the year must be a leap year
-if( $year % 4 == 0 && $year % 100 != 0 ) {
-    print("$year is a leap year!");
-} elsif (year % 400 == 0) {
+if( $year % 4 == 0 ) {
+	if( $year % 100 != 0 )
+    {
+    	print("$year is a leap year!");
+    }
+} elsif ($year % 400 == 0) {
     print("$year is a leap year!");
 } else {
     print("$year is not a leap year!");
 }
+
