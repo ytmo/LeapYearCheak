c60a68eac446158155fd8a007b81cdd2a235f88f
TomsProject <44722363+TomsProject@users.noreply.github.com>
2020-07-19 14:52:01 +0800

    Update lyc-mac.sh

--- lyc-mac.sh
+++ lyc-mac.sh
@@ -8,7 +8,8 @@ wget -O cmd-only.cpp https://git.io/lyc-cmd.cpp
 
 clang++ cmd-only.cpp -o lyc -std=c++17
 
-sudo cp lyc /usr/bin/lyc
+sudo cp lyc /Applications/lyc
+export PATH=$PATH:/Applications
 rm lyc
 rm cmd-only.cpp
 
