3bcab044142f431b6b1a435610a61907b4cefb0d
TomsProject <44722363+TomsProject@users.noreply.github.com>
2020-07-19 14:00:33 +0800

    Create lyc-mac.sh

---
+++ lyc-mac.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+brew install wget
+        
+wget -O cmd-only.cpp https://git.io/lyc-cmd.cpp
+
+clang++ cmd-only.cpp -o lyc
+
+sudo cp lyc /usr/bin/lyc
+rm lyc
+rm cmd-only.cpp
+
+lyc -v
