05abc068dcff04358341dfb60580319a0816838f
TomsProject <44722363+TomsProject@users.noreply.github.com>
2020-08-05 19:01:57 +0800

    [Wrong outputs now]

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -25,7 +25,8 @@ function readSyncByRl(tips) {
 }
  
 readSyncByRl("Please input a year, and I'll check is that a leap year!!!").then((year) => {
-	if(year % 4 == 0 && year % 100 != 0)    //we learned from math that if a year remainder 4 equals 0 and that year remainder 100 don't equals 0 or the year remainder 400 equals 0, than the year must be a leap year
+    //we learned from math that if a year remainder 4 equals 0 and that year remainder 100 don't equals 0 or the year remainder 400 equals 0, than the year must be a leap year
+    if(year % 4 == 0 && year % 100 != 0)    
     {
         console.log(year + " is a leap year!");
     }
