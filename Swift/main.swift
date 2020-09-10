c4591f62337ecc2c629996a0a10816b8a42d4ed9
TomsProject <44722363+TomsProject@users.noreply.github.com>
2020-09-10 19:37:56 +0800

    [Successed]

--- Swift/main.swift
+++ Swift/main.swift
@@ -9,7 +9,8 @@ import Foundation     //for print and input
 
 print("Please input a year, and I'll check is that a leap year!!!")
 let year = Int(readLine()!)
-if(year! % 4 == 0 && year! % 100 != 0)    //we learned from math that if a year remainder 4 equals 0 and that year remainder 100 don't equals 0 or the year remainder 400 equals 0, than the year must be a leap year
+//we learned from math that if a year remainder 4 equals 0 and that year remainder 100 don't equals 0 or the year remainder 400 equals 0, than the year must be a leap year
+if(year! % 4 == 0 && year! % 100 != 0)    
 {
 	print(String(year!) + " is a leap year!")
 }
