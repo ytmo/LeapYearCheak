77c833d257fac4fceca18e736d195296c95d30c7
TomsProject <44722363+TomsProject@users.noreply.github.com>
2021-02-07 12:17:40 +0800

    [Succeed]

--- Objective-C/main.m
+++ Objective-C/main.m
@@ -3,29 +3,26 @@
 //  LeapYearCheak
 //
 //  Created by Toms Project on 2020/6/13.
-//  Copyright © 2020 Toms Project. All rights reserved.
+//  Copyright © 2021 Toms Project. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        NSLog(@"Please input a year, and I'll check is that a leap year!!!");
-        int l[100];
-        scanf("%d",l);            //input the year we want to check
-        int year = l;
-        if(year % 4 == 0 && year % 100 != 0)    //we learned from math that if a year remainder 4 equals 0 and that year remainder 100 don't equals 0 or the year remainder 400 equals 0, than the year must be a leap year
-        {
-            printf("%d is a leap year! \n",&year);
-        }
-        else if (year % 400 == 0)
-        {
-            printf("%d is a leap year! \n",&year);
-        }
-        else
-        {
-            printf("%d is not a leap year! \n",&year);
-        }
+    printf("Please input a year, and I'll check is that a leap year!!! \n");
+    int year = 0;
+    scanf("%d", &year);            //input the year we want to check
+    if(year % 4 == 0 && year % 100 != 0)    //we learned from math that if a year remainder 4 equals 0 and that year remainder 100 don't equals 0 or the year remainder 400 equals 0, than the year must be a leap year
+    {
+        printf("%d is a leap year! \n", year);
+    }
+    else if (year % 400 == 0)
+    {
+        printf("%d is a leap year! \n", year);
+    }
+    else
+    {
+       printf("%d is not a leap year! \n", year);
     }
     return 0;
 }
